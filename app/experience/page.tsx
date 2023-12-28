2b24c235ef68efba7448ea09941eb1927b526b9e
Naman Gulati <naman.gulati@loconav.com>
2023-12-28 17:07:56 +0530

    add nextjs project, dependencies, layout and routes for portfolio

---
+++ app/experience/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExperiencePage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>ExperiencePage</h1>
+    </main>
+  )
+}
+
+export default ExperiencePage
